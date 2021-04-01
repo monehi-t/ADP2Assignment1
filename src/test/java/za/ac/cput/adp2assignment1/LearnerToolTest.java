920c754dfd5749c09225fad2b1ff53213da68d41
Mike <mikesomelezotyolani41@gmail.com>
2021-04-01 21:04:29 +0200

    Added program and did TDD.

--- src/test/java/za/ac/cput/adp2assignment1/LearnerToolTest.java
+++ src/test/java/za/ac/cput/adp2assignment1/LearnerToolTest.java
@@ -5,20 +5,26 @@
  */
 package za.ac.cput.adp2assignment1;
 
+import java.time.Duration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Disabled;
 
 /**
  *
  * @author CPUT
  */
-public class ApplicationTest {
+public class LearnerToolTest {
+    private LearnerTool tool1;
+    private LearnerTool tool2;
+    private LearnerTool tool3;
+    private LearnerTool tool4;
     
-    public ApplicationTest() {
+    public LearnerToolTest() {
     }
     
     @BeforeAll
@@ -31,13 +37,43 @@ public class ApplicationTest {
     
     @BeforeEach
     public void setUp() {
+       tool1 = new LearnerTool();
+       tool2 = tool4;
+       tool3 = new LearnerTool();
     }
     
     @AfterEach
     public void tearDown() {
     }
+     @Test        
+    void testEquality()
+    {
+        assertEquals(true,tool1.Valid("Blackboard"));
+    }
+    @Test
+    void testIdentity()
+    {
+       
+        assertSame(tool2,tool4);
+    }
+    
+    @Test
+    void testFailing()
+    {
+       fail("Failing Test");
+        assertSame(true,tool3.Valid(""));
+    
+    }
+    
+   @Test
+   void TimeoutExceed ()
+   {
+       assertTimeout(Duration.ofMillis(17), () -> Thread.sleep(10));
+   
+   }
 
     @Test
+    @Disabled
     public void testSomeMethod() {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
